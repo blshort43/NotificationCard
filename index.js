8557ea7a82f2a5c1aac2ae2fc94e8ab299bfd2aa
Brandon <blshort43@users.noreply.github.com>
2019-09-09 11:09:10 -0500

    focus animations added
    Focus animations now work when user tabs to card.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Box, TranslateText, Card } from 'components';
@@ -10,8 +11,9 @@ const StyledCard = styled(Card)`
   :hover {
     box-shadow: 2px 5px 4px #888888;
     transform: translate(0px, -4px);
-    transition: ease 0.2s;
   }
+  box-shadow: ${props => (props.focus ? '2px 5px 4px #888888' : 'none')};
+  transform: ${props => (props.focus ? 'translate(0px, -4px)' : 'none')};
   transition: ease 0.2s;
 `;
 
@@ -38,25 +40,41 @@ const NotificationCard = ({
   total,
   translate,
   title,
+  to,
+  focus,
   ...rest
-}) => (
-  <Box width={[1 / 4]} p={1}>
-    <StyledCard {...rest} p={3}>
-      <Box m="auto">
-        {icon &&
-          React.cloneElement(icon, {
-            size: icon.props.size || 50,
-            color: 'white',
-          })}
-      </Box>
-      <TextContainer width={[3 / 4]} p={1}>
-        {children}
-        <TitleText translate={title} />
-        <TotalText>{total || 0}</TotalText>
-      </TextContainer>
-    </StyledCard>
-  </Box>
-);
+}) => {
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+
+  const handleBlur = () => {
+    setFocused(false);
+  };
+
+  return (
+    <Box onFocus={handleFocus} onBlur={handleBlur} width={[1 / 4]} p={1}>
+      <Link to={`app/${to}`} style={{ textDecoration: 'none' }}>
+        <StyledCard {...rest} focus={focused} p={3}>
+          <Box m="auto">
+            {icon &&
+              React.cloneElement(icon, {
+                size: icon.props.size || 50,
+                color: 'white',
+              })}
+          </Box>
+          <TextContainer width={[3 / 4]} p={1}>
+            {children}
+            <TitleText translate={title} />
+            <TotalText>{total || 0}</TotalText>
+          </TextContainer>
+        </StyledCard>
+      </Link>
+    </Box>
+  );
+};
 
 NotificationCard.propTypes = {
   children: PropTypes.any,
@@ -64,6 +82,8 @@ NotificationCard.propTypes = {
   total: PropTypes.number,
   translate: PropTypes.object,
   title: PropTypes.object,
+  to: PropTypes.string,
+  focus: PropTypes.bool,
 };
 
 export default NotificationCard;
